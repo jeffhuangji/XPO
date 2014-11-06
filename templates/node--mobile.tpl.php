fccdfe321183634c03eeb0edc560de77d461454a
Sause <lazureys@gmail.com>
2014-11-06 22:43:07 +0800

    i forget

--- templates/node--mobile.tpl.php
+++ templates/node--mobile.tpl.php
@@ -81,15 +81,16 @@
  */
 ?>
 <?php 
-$desc = $content['body'];
-$feature = $content['field_phone_feature'];
-$spec = $content['field_specs'];
-$review = $content['field_reviews'];
-$brand = $content['field_brand'];
-$carrier = $content['field_carrier'];
-$category = $content['field_features'];
-$pics = $content['field_image'];
-$carrier_logo = $content['field_carrier_logo'];
+$price = isset($content['field_price_table']) ? $content['field_price_table'] : '';
+$desc = isset($content['body']) ? $content['body'] : '';
+$feature = isset($content['field_phone_feature']) ? $content['field_phone_feature'] : '';
+$spec = isset($content['field_specs']) ? $content['field_specs'] : '';
+$review = isset($content['field_reviews']) ? $content['field_reviews'] : '';
+$brand = isset($content['field_brand']) ? $content['field_brand'] : '';
+$carrier = isset($content['field_carrier']) ? $content['field_carrier'] : '';
+$category = isset($content['field_features']) ? $content['field_features'] : '';
+$pics = isset($content['field_image']) ? $content['field_image'] : '';
+$carrier_logo = isset($content['field_carrier_logo']) ? $content['field_carrier_logo'] : '';
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
@@ -104,11 +105,10 @@ $carrier_logo = $content['field_carrier_logo'];
       <div class="col-md-4"><?php print render($carrier_logo); print render($pics); ?></div>
       <div class="col-md-8">
         <?php 
-          print $title;
-          print render($desc);
+          // print $title;
+          print render($price);
          ?>
          <div class="read-more">
-           <button class="btn btn-default">See more details</button>
          </div>
       </div>
     </div>
@@ -122,19 +122,20 @@ $carrier_logo = $content['field_carrier_logo'];
 
     <div class="tabs">
       <ul class="nav nav-tabs" role="tablist">
-        <li><a href="#general" role="tab" data-toggle="tab">General</a></li>
-        <li class="active"><a href="#feature" role="tab" data-toggle="tab">Features</a></li>
+        <li class="active"><a href="#general" role="tab" data-toggle="tab">General</a></li>
+        <li><a href="#feature" role="tab" data-toggle="tab">Features</a></li>
         <li><a href="#spec" role="tab" data-toggle="tab">Specs</a></li>
         <li><a href="#review" role="tab" data-toggle="tab">Reviews</a></li>
       </ul>
 
       <div class="tab-content">
-        <div class="tab-pane" id="general">
+        <div class="tab-pane active" id="general">
+          <?php print render($desc); ?>
           <?php print render($brand); ?>
           <?php print render($carrier); ?>
           <?php print render($category); ?>    
         </div>
-        <div class="tab-pane active" id="feature"><?php print render($feature); ?></div>
+        <div class="tab-pane" id="feature"><?php print render($feature); ?></div>
         <div class="tab-pane" id="spec"><?php print render($spec); ?></div>
         <div class="tab-pane" id="review"><?php print render($review); ?></div>
       </div>
